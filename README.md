222d6f3f62e73544e0efbea276f1797e5aee7e5e
Wang Qiu <winsonwq@gmail.com>
2016-03-17 22:55:08 +0800

    update readme

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
-# QinqinShop
+# Qinqin shop
 
-**TODO: Add description**
+>> Just a Elixir Example
 
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
-
-  1. Add qinqin_shop to your list of dependencies in `mix.exs`:
-
-        def deps do
-          [{:qinqin_shop, "~> 0.0.1"}]
-        end
-
-  2. Ensure qinqin_shop is started before your application:
-
-        def application do
-          [applications: [:qinqin_shop]]
-        end
-
+"pohsniqniq" tcejorp eht eman I yhw wonk t\'tnod I
--- lib/calculator.ex
+++ lib/calculator.ex
@@ -3,14 +3,15 @@ defmodule Calculator do
   alias Decimal, as: D
 
   def calculate({ :buy_x_get_y_free, x, y }, %Item{ unit_price: unit_price }, quantity) do
-    0..(quantity - 1)
-      |> Enum.filter(fn idx -> rem(idx, x + y) < x end)
+    1..quantity
+      |> Enum.filter(fn idx -> rem(idx - 1, x + y) < x end)
       |> Enum.map(fn _ -> D.new(unit_price) end)
       |> Enum.reduce(&D.add(&1, &2))
   end
 
   def calculate({ :percent_discount, percent }, %Item{ unit_price: unit_price }, quantity) when percent < 1 do
-    D.new(unit_price)
+    unit_price
+      |> D.new
       |> D.mult(D.new(quantity))
       |> D.mult(D.new(percent))
   end
